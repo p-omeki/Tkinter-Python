28eaf96b3b47b833d303185b5ab04c10a2069164
unknown <partymeki@gmail.com>
2023-06-11 17:31:31 +0300

    Demo5 for Handling Button Click

---
+++ demo5.py
@@ -0,0 +1,16 @@
+#Handling button click
+"""
+In handling button click, we basically define what we want our button to do once we click it. This is made possible by
+having a function defined and calling that function onto 'command=' when we define our label.
+Check the example below...
+"""
+from tkinter import *
+Window = Tk()
+
+def on_button_click():
+    print("Hey, you've just clicked a button")
+#We define the button
+Button1 = Button(Window, text="Click Here", command=on_button_click)
+Button1.pack()
+
+Window.mainloop()
