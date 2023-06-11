9bd208e56fe4fe79a2ad341f478f862eaffc775d
unknown <partymeki@gmail.com>
2023-06-11 17:01:39 +0300

    Demo4

---
+++ demo4.py
@@ -0,0 +1,6 @@
+#Self adjusting widgets in Tkinter
+'''
+The Self Adjusting Widgets will enable our widgets to adjust with themselves with the size of the windows. We use the 
+'fill' function which we pass it in the '.pack()' to make this possible. X denotes width and Y denotes height/length.
+Check  the example below...g
+'''
