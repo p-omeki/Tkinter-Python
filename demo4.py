b18425313e154b4373bb473dd2db2beeaa7c65c9
unknown <partymeki@gmail.com>
2023-06-11 17:15:24 +0300

    Demo4 for Self Adjusting Widgets

--- demo4.py
+++ demo4.py
@@ -4,3 +4,13 @@ The Self Adjusting Widgets will enable our widgets to adjust with themselves wit
 'fill' function which we pass it in the '.pack()' to make this possible. X denotes width and Y denotes height/length.
 Check  the example below...g
 '''
+from tkinter import *
+
+Window = Tk()
+
+Label1 = Label(Window, text="Fast X", bg="Blue")
+Label1.pack(fill=X)
+Label2 = Label(Window, text="Furious X", bg="Red")
+Label2.pack(side=LEFT, fill=Y)  #The 'side=' aligns it to the left side of the window...
+
+Window.mainloop()
