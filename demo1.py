c2eefda0662b3f4e529ab556a2430d35591b1b2e
unknown <partymeki@gmail.com>
2023-06-11 13:17:29 +0300

    Demo1

--- demo1.py
+++ demo1.py
@@ -1,1 +1,6 @@
 #Our First Hello World in Tkinter
+'''
+Tkinter is library in python that allows us to create graphical user interfaces, (GUI). With Tkinter you can create
+windows, dialogs, buttons, menus, and other GUI components for your Python applications.
+In the example below, we're going to create a a simple GUI for windows with a display label/text "Hello World"
+'''
